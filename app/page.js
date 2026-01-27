af90aa803bd5c7f03ea96a25116c8758b8afca40
Messinaldo10 <albert.calvet1@gmail.com>
2026-01-27 15:12:21 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "system-ui" }}>
+      <h1>Wancko</h1>
+      <p>Natural assistant aligned with AU.</p>
+    </main>
+  );
+}
